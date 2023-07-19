e808360c96de0e329029fc4113de968c792ce029
Marshall Ku <marshall@kakao.com>
2023-07-19 09:01:45 +0900

    Add error handling for webview or old browsers

--- src/libs/copyToClipboard.ts
+++ src/libs/copyToClipboard.ts
@@ -1,4 +1,15 @@
 export default async function copyToClipboard(value: string, cb?: () => void) {
-    await navigator.clipboard.writeText(value);
+    try {
+        await navigator.clipboard.writeText(value);
+    } catch {
+        const textarea = document.createElement("textarea");
+
+        textarea.value = value;
+        document.body.append(textarea);
+        textarea.focus();
+        textarea.select();
+        document.execCommand("copy");
+        textarea.remove();
+    }
     cb?.();
 }
